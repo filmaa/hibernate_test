0b8841908eabeb8d3495835a91870a1a0f4c7e47
katsiaryna <kate.filmanovich@gmail.com>
2018-03-20 18:19:00 +0300

    add script creating whole database

--- src/main/java/test/hibernate/util/HibernateUtil.java
+++ src/main/java/test/hibernate/util/HibernateUtil.java
@@ -3,6 +3,9 @@ package test.hibernate.util;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+
+import test.hibernate.entity.Person;
 
 public class HibernateUtil {
     private static final SessionFactory sessionFactory;
@@ -11,6 +14,9 @@ public class HibernateUtil {
         try {
             // create the SessionFactory from hibernate.cfg.xml
             sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+            Configuration aconf = new AnnotationConfiguration().
+                    addAnnotatedClass(Person.class);
+            aconf.configure();
         } catch (Throwable e) {
             // make sure you log the exception, as it might be swallowedHibernate
             System.err.println("Initial SessionFactory creation failed." + e);
